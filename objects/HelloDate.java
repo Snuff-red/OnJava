c29ee595cab1da96e1dc0aeaac936e8066d64ecc
Snuff-red <Snuff-red1900@outlook.com>
2022-06-21 23:15:25 +0800

    OnJava

---
+++ objects/HelloDate.java
@@ -0,0 +1,9 @@
+//  objects/HelloDate.java
+import  java.util.*;
+
+public  class HelloDate{
+  public  static  void  main(String[] args){
+    System.out.print("Hello,it's:");
+    System.out.println(new Date());
+  }
+}
